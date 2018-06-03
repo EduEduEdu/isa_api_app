cb2e83d677ccb399d43380c4b92566014a23d140
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2018-06-03 09:44:32 -0300

    first commit

---
+++ src/main/java/com/gerdau/isa_api_app/IsaApiAppApplication.java
@@ -0,0 +1,12 @@
+package com.gerdau.isa_api_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApiAppApplication.class, args);
+	}
+}
